ab5cfc6ede602a64a3e7ff2c06e75ac2f1a8bacf
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-16 20:41:35 -0600

    userdata

--- instance.tf
+++ instance.tf
@@ -3,6 +3,9 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro" 
   associate_public_ip_address = "true" 
   key_name= aws_key_pair.deployer.key_name
+
+  user_data = file("userdata_file")
+  
 tags = { 
     Name = "HelloWorld" 
   } 
