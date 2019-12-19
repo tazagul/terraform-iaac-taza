9e7f6a55da668fbe832cbb723f964f217102d150
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-18 18:26:43 -0600

    ssh

--- sg.tf
+++ sg.tf
@@ -6,7 +6,7 @@ resource "aws_security_group" "allow_ssh" {
   ingress { 
     from_port   = 22 
     to_port     = 22 
-    protocol    = "http" 
+    protocol    = "ssh" 
     cidr_blocks = ["0.0.0.0/0"]   
 
 } 
