80ec591e2ec20742b656c6307b512a18c6d0e525
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-18 19:08:03 -0600

    output added

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
-output "backet_name" {}
-value = aws_s3_bucket.b.bucket 
+output "backet_name" {
+    value = aws_s3_bucket.b.bucket 
 }
+
+
+output "key name" {}
+    value = aws_key_pair.deployer.key_name 
+}
+
