d39223646ce19bacf50b57ff81cc1fcd0c3d513c
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-20 22:57:42 -0600

    s3

--- s3.tf
+++ s3.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "b" { 
-  bucket = "my-tf-test-bucket" 
+  bucket = "my-tf-test-bucket-tazagul" 
   acl    = "private" 
 
   tags = { 
     Name        = "My bucket" 
     Environment = "Dev" 
   } 
-} 
+
+  versioning {
+    enabled =tre
+  }
+}
