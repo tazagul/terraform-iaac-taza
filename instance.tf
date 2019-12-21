1ddb5262d16a89b495956c67cce45978b4b4fb16
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-21 12:32:11 -0600

    copy

--- instance.tf
+++ instance.tf
@@ -5,25 +5,24 @@ resource "aws_instance" "web" {
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
-  provisioner "remote-exec" { 
-    connection { 
-     host        = self.public_ip
-     type        = "ssh" 
-     user        = var.user
-     private_key = file(var.ssh_key_location) 
-    }  
-    inline = [ 
-      "sudo yum install -y epel-release", 
-      "sudo yum install httpd -y"
-      "sudo systemctl start httpd"
-    ] 
-  } 
-
- 
+  provisioner "remote-exec" {
+    connection {
+      host        = self.public_ip
+      type        = "ssh"
+      user        = var.user
+      private_key = file(var.ssh_key_location)
+      }
+      inline = [
+        "sudo yum install -y epel-release",
+        "sudo yum install httpd -y ",
+        "systemctl start httpd",
+        ]
+      }
   lifecycle{
     prevent_destroy = false
   }
   tags = {
     Name = "HelloWorld${count.index +1}"
   }
+}
 }
