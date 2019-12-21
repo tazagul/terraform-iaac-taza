bcd0786afd98e2985a688cd277c376c60c528a0d
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-21 11:48:55 -0600

    added count

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
-  count = 10
+  count = var.count
   ami             = var.ami
-  instance_type   = "t2.micro"
+  instance_type   = var.instance_type
   associate_public_ip_address = "true"
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
--- variables.tf
+++ variables.tf
@@ -1,1 +1,3 @@
 variable "ami" {}
+variable "instance_type" {}
+variable "count" {}
