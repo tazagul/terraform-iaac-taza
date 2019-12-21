d8535ee50e6d4d5ea84425489169f9489ddb8ee7
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2019-12-21 12:26:54 -0600

    provisioner

--- instance.tf
+++ instance.tf
@@ -5,7 +5,21 @@ resource "aws_instance" "web" {
   associate_public_ip_address = var.associate_public_ip_address
   key_name = aws_key_pair.deployer.key_name
   security_groups = ["allow_ssh"]
-  user_data = file("userdata_file")
+  provisioner "remote-exec" { 
+    connection { 
+      host        = self.public_ip
+      type        = "ssh" 
+      user        = var.user
+      private_key = file(var.ssh_key_location) 
+    } 
+    inline = [ 
+      "sudo yum install -y epel-release", 
+      "sudo yum install httpd -y"
+      "sudo systemctl start httpd"
+    ] 
+  } 
+
+ 
   lifecycle{
     prevent_destroy = false
   }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "ami" {}
 variable "instance_type" {}
 variable "count_instance" {}
 variable "associate_public_ip_address" {}
+variable "user" {}
+variable "ssh_key_location" {
+  
+}
+
