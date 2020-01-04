33230dc9b27f1e7fa492fd8f60822bf091a70624
Tazagul Kumarova <tazagulkumarova96@gmail.com>
2020-01-04 09:28:54 -0800

    role added

--- aws/iam.tf
+++ aws/iam.tf
@@ -26,4 +26,38 @@ resource "aws_iam_group_membership" "team" {
 }
 
 
- 
+ resource "aws_iam_role" "test_role" { 
+
+name = "test_role" 
+
+assume_role_policy = <<EOF 
+
+{ 
+
+  "Version": "2012-10-17", 
+
+  "Statement": [ 
+
+{ 
+
+  "Action": "sts:AssumeRole", 
+
+  "Principal": { 
+
+  "Service": "ec2.amazonaws.com" 
+
+}, 
+
+  "Effect": "Allow", 
+
+  "Sid": "" 
+
+    } 
+
+  ] 
+
+} 
+
+EOF 
+
+} 
